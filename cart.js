d353d8d1c99928a2ec8bd9c137dd1fac4805e51b
charayo <69734231+charayo@users.noreply.github.com>
2021-03-17 08:59:13 +0000

    update

---
+++ cart.js
@@ -0,0 +1,48 @@
+(function ($) {
+    let cart = [];
+
+    if (localStorage.getItem('cartItems')) {
+        cart = JSON.parse(localStorage.getItem('cartItems'));
+        document.getElementById("cartVal").innerHTML = `${cart.length} ITEMS`;
+        for (let index = 0; index < cart.length; index++) {
+            document.getElementById("cartJsTarg").innerHTML += (`
+            <div class="container card mt-2 mb-3 shadow-lg w-50" id="cartCard${index}">
+                <div class="">
+                    <div class="row mb-2 text-center">
+                        <div class="col-md-4 text-cen" id="imageTarget">
+                            <img id="imageId" class="w-75 mt-4" src="${cart[index].itemImgSrc}" alt="product-image">
+                        </div>
+                        <div class="col-md-8" id="descrTarget">
+                            <div class="pt-4">
+                                <h4 class="">${cart[index].itemName}</h4>
+                                <p class="">${cart[index].itemPrice}</p>
+                                <hr>
+                                <input type="button" value="Remove from Cart" class="btn btn-danger removeFromCart" id="removeFromCart">
+                            </div>                                
+                        </div>
+                    </div>
+                </div>
+            </div>
+        `)
+        }
+    }
+    // console.log(cart);
+    let rmvBtn = document.querySelectorAll(".removeFromCart");
+    rmvBtn.forEach((element,index) => {        
+        element.addEventListener('click', () => {
+            cart.splice(index,1);
+            localStorage.setItem("cartItems", JSON.stringify(cart));
+            localStorage.setItem("orderedItems", JSON.stringify(cart));
+            $(`#cartCard${index}`).remove();
+             
+            cartNum = localStorage.getItem('cartCount');
+            localStorage.removeItem('cartCount')
+            cartNum--;
+            document.getElementById("cartVal").innerHTML = `${cartNum} ITEM(S)`; 
+            localStorage.setItem("cartCount", cartNum);
+            localStorage.setItem("orderedCount", cartNum);
+            $("#cartUpdate").html(cartNum);
+            console.log(cart);
+        })
+    });
+})(jQuery);
--- main.js
+++ main.js
@@ -2,7 +2,20 @@
     let cartValue = $("#cartUpdate").html();
     let cartItem = [];
     let i = 0;
-    // let fromLocalStorage;
+    if (localStorage.getItem('logged')) {
+        if (localStorage.getItem('user')) {
+            let userDetails = JSON.parse(localStorage.getItem('user'));
+            $('#navbarDropdown').html(userDetails.userId);
+            $('#signOut').removeClass('d-none');
+            $('#indexCrtAcc').addClass('d-none');
+            $('#indexLogin').addClass('d-none');
+
+        }
+    }
+    if(localStorage.getItem('user') && !localStorage.getItem('logged')){
+        $('#myModal').modal('show');
+    }
+
     if (localStorage.getItem('cartItems')) {
         cartItem = JSON.parse(localStorage.getItem('cartItems'));
     }
@@ -20,7 +33,7 @@
         $('body').delay(450).css({ 'overflow': 'visible' });
     });
 
-
+    //For the Site Theme
     $("#toggleTim").on("click", () => {
         localStorage.getItem('theme')
         $('#toggleTim').toggleClass('btn-light');
@@ -41,24 +54,30 @@
             localStorage.setItem('theme', false)
         }
     })
+    //------------------------------
+
+    //The sign-up page
     $("#formCancelBtn").on("click", () => {
         // window.location.href="index.html";
         alert('closed');
         window.location.href = "index.html";
     })
-    $("#formSignupBtn").click(() => {
-        alert('Succesfull Sign-up; Welcome!')
-        window.location.href = "index.html";
-    })
-    $("#crtAccBtn").click(() => {
-        window.location.href = "signup.html";
-    })
+    //Craeting a user account
+    // $("#formSignupBtn").click(() => {
+    //     window.location.href = "index.html";
+    // })
+    // $("#crtAccBtn").click(() => {
+    //     window.location.href = "signup.html";
+    // })
+    //--------------------------
+
+    //Adding items into the cart
     $("#addToCart").on("click", () => {
         i++;
         let itemName = $("#itemName").html();
         let itemPrice = $("#itemPrice").html();
         let imageSrc = $("#imageId").attr('src');
-        
+
         let item = {
             itemName: itemName,
             itemPrice: itemPrice,
@@ -66,6 +85,7 @@
         }
         cartItem.push(item);
         localStorage.setItem("cartItems", JSON.stringify(cartItem));
+        localStorage.setItem("orderedItems", JSON.stringify(cartItem));
 
         if (localStorage.getItem('cartCount')) {
             cartValue = localStorage.getItem('cartCount');
@@ -73,12 +93,89 @@
         cartValue++;
         $("#cartUpdate").html(cartValue);
         localStorage.setItem("cartCount", cartValue)
-        console.log(cartItem);
+        localStorage.setItem("orderedCount", cartValue)
     })
-    $('#loginBtn').on('click',()=>{
+    //-------------------------------
+
+    //For Login Page
+    $('#loginBtn').on('click', () => {
         let name = $('#userIdInp').val();
         alert('welcome back! ' + name);
     })
+    //------------------------
 
 
+    // For Payment page
+    $('#payBtn').on('click', () => {
+        // alert('pay');
+        if ($('#snInp').val().length >= 12 && $('#yearInp').val().length >= 4 && $('#cvvInp').val().length == 3) {
+            let num = 10;
+            let redirect = () => {
+                location.href = 'index.html'
+            }
+            localStorage.removeItem('cartItems')
+            localStorage.removeItem('cartCount')
+            localStorage.setItem('orderMade', true)
+            $('#alert').removeClass('d-none');
+            setTimeout(redirect, 10000);
+            let countDwn = () => {
+                $('#countDwn').html(num);
+                if (num > 0) {
+                    num--;
+                }
+            }
+            let myTime = setInterval(countDwn, 1000);
+            if (num == 0) {
+                clearInterval(myTime);
+            }
+
+        } else {
+            alert('please fill properly');
+        }
+    })
+    //-----------------------------------
+
+    //For signout button
+    $('#signOutBtn').on('click', () => {
+        // localStorage.removeItem('user');
+        window.location.href = "index.html";
+        $('#navbarDropdown').html('Login');
+        $('#signOut').addClass('d-none');
+        $('#indexCrtAcc').removeClass('d-none');
+        $('#indexLogin').removeClass('d-none');
+
+    })
+    //-----------------
+    //For Sign up Page
+    let getUserDetails = () => {
+        let password = $('#userPass').val();
+        let email = $('#userEmail').val();
+        let username = $('#userId').val();
+        let passVali = /(?=.*[0-9])(?=.*[-_@])(?=.*[A-Z])[a-zA-Z0-9@-_@]{8,16}$/;
+        let test = passVali.test(password);
+        let mailVali = /[a-zA-Z0-9]{3,15}[@]{1}[a-zA-Z]{3,10}[.]{1}[a-zA-Z]{2,4}$/
+        let mailCheck = mailVali.test(email);
+        if (test && mailCheck) {
+            let userDetails = {
+                userPass: password,
+                userEmail: email,
+                userId: username,
+            }
+            console.log(userDetails);
+            localStorage.setItem('user', JSON.stringify(userDetails));
+            window.location.href = "index.html";
+            
+
+        } else {
+            $('#signUpForm').effect('shake');
+            $('#wrongPassAlert').removeClass('d-none');
+        }
+    }
+    $('#formSignupBtn').on('click', () => {
+        getUserDetails();
+    })
+    //---------------------------
+    // $('#prom').on('click', ()=>{
+    //     alert();
+    // })
 })(jQuery);
